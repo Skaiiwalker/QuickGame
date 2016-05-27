064dbb941c034d5f0f5c4412d14931955c75cfb3
Skaiiwalker <lbar8299@740s-pl-369.local>
2016-05-27 11:56:33 -0600

    Followed more tutorial

--- QuickGame/Controller/QuickGame.cs
+++ QuickGame/Controller/QuickGame.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 
 namespace QuickGame.Controller
 {
@@ -13,16 +14,26 @@ namespace QuickGame.Controller
 		SpriteBatch spriteBatch;
 		Player player;
 
+		KeyboardState currentKeyboardState;
+		KeyboardState previousKeyboardState;
+
+		GamePadState currentGamePadState;
+		GamePadState previousGamePadState;
+
+		float playerMoveSpeed;
+
 		public QuickGame ()
 		{
 			graphics = new GraphicsDeviceManager (this);
 			Content.RootDirectory = "Content";
-
 		}
 			
 		protected override void Initialize ()
 		{
 			player = new Player ();
+			playerMoveSpeed = 8.0f;
+			TouchPanel.EnabledGestures = GestureType.FreeDrag;
+
 			base.Initialize ();
 		}
 			
@@ -43,6 +54,13 @@ namespace QuickGame.Controller
 				Exit ();
 			#endif
             
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
+
+			currentKeyboardState = Keyboard.GetState ();
+			currentGamePadState = GamePad.GetState (PlayerIndex.One);
+
+			UpdatePlayer (gameTime);
 			base.Update (gameTime);
 		}
 			
@@ -56,6 +74,37 @@ namespace QuickGame.Controller
 
 			base.Draw (gameTime);
 		}
+
+		private void UpdatePlayer(GameTime gameTime)
+		{
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+			if(currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+
+			if(currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+
+			if(currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+
+			if(currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			player.Position.X = MathHelper.Clamp (player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp (player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+		}
+
+
 	}
 }
 
---
+++ QuickGame/View/Animation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace QuickGame
+{
+	public class Animation
+	{
+		Texture2D spriteStrip;
+		float scale;
+
+		int elapsedTime;
+		int frameTime;
+		int frameCount;
+		int currentFrame;
+
+		Color color;
+		Rectangle sourceRect = new Rectangle();
+		Rectangle destinationRect = new Rectangle();
+
+		public int FrameWidth;
+		public int FrameHeight;
+
+		public bool Active;
+		public bool Looping;
+		public Vector2 Position;
+
+		public void Initializate()
+		{
+			
+		}
+
+		public void Update()
+		{
+			
+		}
+
+		public void Draw()
+		{
+			
+		}
+
+		public Animation ()
+		{
+			
+		}
+	}
+}
+
